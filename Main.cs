35af918737f3d88216215dff6a636e966a120152
Krelinos <emmer.palaming@gmail.com>
2024-12-12 21:01:38 -0600

    The start of the map generation

--- Main.cs
+++ Main.cs
@@ -7,25 +7,55 @@ public class Main : Node2D
     // Rooms are organized onto a 2D grid.
     private Dictionary< Vector2, Room > Rooms;
 
+    private String Seed
+    {
+        get => SeedInput.Text;
+    }
+
+
+    private readonly RandomNumberGenerator RNG = new RandomNumberGenerator();
+
+    private LineEdit SeedInput;
+    private SpinBox IndexInput;
+    private Button GenerateButton;
+    private Node ConnectorLayer;
+    private Node RoomLayer;
+
     private PackedScene _Room;
 
     public override void _Ready()
     {
+        SeedInput = GetNode<LineEdit>("Toolbar/HBoxContainer/Seed");
+        IndexInput = GetNode<SpinBox>("Toolbar/HBoxContainer/Index");
+        GenerateButton = GetNode<Button>("Toolbar/HBoxContainer/Button");
+
+        ConnectorLayer = GetNode("Origin/Connectors");
+        RoomLayer = GetNode("Origin/Rooms");
+
         _Room = GD.Load("res://Scenes/Room.tscn") as PackedScene;
+        
+        RNG.Seed = Seed.Hash();
+        RNG.State = (ulong)IndexInput.Value;
+
+        GenerateButton.Connect( "pressed", this, nameof(OnGenerateButtonPressed) );
+    }
+
+    private Vector2 GetRandomOpenCoord()
+    {
+        Vector2 result;
+        do
+            result = new Vector2( RNG.RandiRange(-4, 4), RNG.RandiRange(-4, 4) );
+        while ( Rooms.ContainsKey( result ) );
+
+        return result;
     }
 
     private Room CreateRoom( Vector2 coord, String type )
     {
         if ( Rooms.ContainsKey( coord ) )
         {
-            GD.PushError("Tried to generate a room at (" + coord + "), but it already has one.");
+            GD.PushError("Tried to generate a room at (" + coord + "), but one already exists there.");
             return null;
-        }
-
-        if ( !Room.ROOM_COLORS.ContainsKey( type ) )
-        {
-            GD.PushError("Unknown room type '" + type + "' was provided. Defaulting to 'none'.");
-            type = "none";
         }
 
         var room = _Room.Instance() as Room;
@@ -37,6 +67,32 @@ public class Main : Node2D
 
     private void PopulateMap()
     {
-        
+        RNG.State = (ulong)IndexInput.Value;
+
+        // Clear existing elements on the map, then setup for a new batch.
+        foreach( Node connector in ConnectorLayer.GetChildren() )
+            connector.QueueFree();
+        foreach( Node room in RoomLayer.GetChildren() )
+            room.QueueFree();
+        Rooms = new Dictionary<Vector2, Room>();
+
+        // Every map has a max of one Start and one Goal.
+        Vector2 randCoord = GetRandomOpenCoord();
+        var start = CreateRoom( randCoord, "start" );
+        Rooms[ start.Coordinates ] = start;
+
+        randCoord = GetRandomOpenCoord();
+        var goal = CreateRoom( randCoord, "goal" );
+        Rooms[ goal.Coordinates ] = goal;
+
+        RoomLayer.AddChild( start );
+        RoomLayer.AddChild( goal );
+
+        IndexInput.Value++;
+    }
+
+    private void OnGenerateButtonPressed()
+    {
+        PopulateMap();
     }
 }
--- Room.cs
+++ Room.cs
@@ -7,17 +7,42 @@ public class Room : Node2D
     public static readonly Dictionary<String, Color> ROOM_COLORS = new Dictionary<String, Color>
     {
         { "none", Colors.GhostWhite }
-        ,{ "start", Colors.Honeydew }
-        ,{ "goal", Colors.FloralWhite }
+        ,{ "start", Colors.DarkGoldenrod }
+        ,{ "goal", Colors.PeachPuff }
         ,{ "good", Colors.ForestGreen }
         ,{ "bad", Colors.LightCoral }
-        ,{ "teleport", Colors.MediumOrchid }
+        ,{ "teleport_random", Colors.MediumOrchid }
+        ,{ "teleport_pair_a", Colors.DarkTurquoise }
+        ,{ "teleport_pair_b", Colors.LightGreen }
         ,{ "shop", Colors.RoyalBlue }
-        ,{ "shadowrealm", Colors.MediumPurple }
+        ,{ "shadow_realm", Colors.MediumPurple }
     };
 
-    public Vector2 Coordinates { get; set; }
-    public String Type { get; set; }
+    public Vector2 Coordinates
+    {
+        get => _Coordinates;
+        set
+        {
+            _Coordinates = value;
+            Position = value * 100;
+        }
+    }
+    public String Type
+    {
+        get => _Type;
+        set
+        {
+            _Type = value;
+
+            if ( !Room.ROOM_COLORS.ContainsKey( value ) )
+            {
+                GD.PushError("Unknown room type '" + value + "' was provided. Defaulting to 'none'.");
+                _Type = "none";
+            }
+
+            //RoomColor = ROOM_COLORS[ _Type ];
+        }
+    }
     public Color RoomColor
     {
         get => LabelBox.Modulate;
@@ -31,10 +56,15 @@ public class Room : Node2D
 
     private NinePatchRect LabelBox;
     private Label Label;
+    private Vector2 _Coordinates;
+    private String _Type = "none";
 
     public override void _Ready()
     {
         LabelBox = GetNode<NinePatchRect>("Label/MarginContainer/NinePatchRect");
         Label = GetNode<Label>("Label");
+
+        RoomColor = ROOM_COLORS[ Type ];
+        Text = Type == "none" ? "" : Type.Capitalize();
     }
 }
